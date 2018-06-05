aa9183a461434dddaa9436f5ab82ba195b36cb74
Anh Do <doducanh2710@gmail.com>
2018-06-05 10:59:45 +0700

    Add dummy C program
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

---
+++ build_c_program.sh
@@ -0,0 +1,1 @@
+gcc -o c_program c_program.c go_second_dummy_lib_for_c.a
---
+++ c_program.c
@@ -0,0 +1,10 @@
+#include "go_second_dummy_lib_for_c.h"
+#include <stdio.h>
+
+int main() {
+    printf("This is a C Application.\n");
+    printf("%s", Say_hello());
+
+    return 0;
+}
+
